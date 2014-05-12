819d0648e8d0fc1486238ef44067a892f943ef0e
frombeijingwithlove <niubi@asu.edu>
2014-05-12 23:27:40 +0800

    Create main.py
    main()

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+A crappy Python implementation of intelligent scissors/livewire
+"""
+import sys
+from PyQt4 import QtGui
+from gui import ImageWin
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    window = ImageWin()
+    window.setMouseTracking(True)
+    window.setWindowTitle('Livewire Demo')
+    window.show()
+    window.setFixedSize(window.size())
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()    
